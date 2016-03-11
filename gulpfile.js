df6ac0ef735c115ab0f67d031bc92f0a61f88e12
rabbitfighter81 <rabbitfighter@cryptolab.net>
2016-03-11 13:08:41 -0700

    Updated gulpfile and file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 /**
- * Gulp multi-language compiler
+ * Gulpfile
  * Author: Joshua Michael Waggoner
  * Email: <rabbitfighter@cryptolab.net>
  */
@@ -9,41 +9,49 @@ var gulp   = require('gulp')
 var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
 var babel  = require('gulp-babel')
+var react  = require('gulp-react')
+var sass   = require('gulp-sass')
 var jshint = require('gulp-jshint')
 var concat = require('gulp-concat')
 
-/**
- * Translate ES6/Babel to EC3 with gulp and place in src/compiled
- */
-gulp.task('babel', function() {
-  return gulp.src('src/*.babel')
-  .pipe(babel())
-  .pipe(gulp.dest('src/compiled'))
-})
 
-
-/**
- * Place all JavaScripts in src in src/compiled
- */
-gulp.task('js', function() {
-  return gulp.src('src/*.js')
-  .pipe(gulp.dest('src/compiled'))
-})
-
-/**
- * Runs Tasks 1-4
- */
- gulp.task('scripts', ['babel', 'js'], function() {
-  return gulp.src(['src/compiled/*.js', ])
-  .pipe(concat('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist'))
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
 });
 
-/**
- * Translates any change to EC3 with gulp and place 
- * in src/compiled and all.min.js
- */
+// Translate EReact to EC3 with gulp and place in src/compiled
+gulp.task('react', function() {
+  return gulp.src('src/*.jsx')
+      .pipe(react())
+      .pipe(gulp.dest('src/compiled'))
+})
+
+// Translate ES6/Babel to EC3 with gulp and place in src/compiled
+gulp.task('babel', function() {
+  return gulp.src('src/*.babel')
+      .pipe(babel())
+      .pipe(gulp.dest('src/compiled'))
+})
+
+
+// Place all JavaScripts in src in src/compiled
+gulp.task('js', function() {
+  return gulp.src('src/*.js')
+      .pipe(gulp.dest('src/compiled'))
+})
+
+// Run all scripts
+ gulp.task('scripts', ['babel', 'react', 'sass', 'js'], function() {
+  return gulp.src(['src/compiled/*.js', ])
+      .pipe(concat('all.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist'))
+});
+
+// Translates any changes with gulp and compiles dist/all.min.js
 gulp.task('watch', function() {
-  gulp.watch('src/*.{js,babel}', ['scripts'])
+  gulp.watch('src/*.{js,babel,jsx}', ['scripts'])
 })
