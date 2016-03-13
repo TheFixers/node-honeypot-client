8c78abb51ad2d06d577b75bcf34fc2f1582f7870
Joshua Michael Waggoner <rabbitfighter@cryptolab.net>
2016-03-13 14:52:11 -0600

    Fixed formatting

--- client/source/components/results-area-component.js
+++ client/source/components/results-area-component.js
@@ -2,31 +2,23 @@ import React from 'react'
 import ClientDataTable from './data-table-component.js'
 
 export default React.createClass({
-	displayName: 'Results Area',
+
+    displayName: 'Results Area',
 	
-	render() {
-		let data = this.props.data
-		return (
-			<div>
-				{Object.keys(data).map(function(key, index) {
-				    return (
-				    	<ClientDataTable 
-				    	    key={index}
-				    	    index={index} 
-				    	    data={data[key]} />
-				    )
-				})}
-			</div>
-		)
+    render() {
+	let data = this.props.data
+	return (
+	    <div>
+		{Object.keys(data).map(function(key, index) {
+		    return (
+		        <ClientDataTable 
+		    	    key={index}
+		    	    index={index} 
+		    	    data={data[key]} />
+		    )
+		})}
+	    </div>
+	)
         
-	}
+    }
 })
-
-/*for (var key in data) {
-// skip loop if the property is from prototype
-    if (!data.hasOwnProperty(key)) continue;
-
-    console.log(key + " = " + JSON.parse(data[key])._id.$oid);
-
-   
-}*/
