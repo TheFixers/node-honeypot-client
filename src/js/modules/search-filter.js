aba3b6ae53b55d2e4761896a93b2d2cc376c8f57
rabbitfighter <rabbitfighter@cryptolab.net>
2016-04-05 09:57:12 -0600

    Fixed indentation in search-filter.js

--- src/js/modules/search-filter.js
+++ src/js/modules/search-filter.js
@@ -5,21 +5,21 @@
  */
 
 const _filtered = ( item, type, term ) => {
-	// If no type or term selected or type is ALL. show all
-	if ( !type || !term || type === 'ALL' ) {
-		return true
+    // If no type or term selected or type is ALL. show all
+    if ( !type || !term || type === 'ALL' ) {
+        return true
     } 
     // Index is a special case where it's a number not a string...
     if ( type == 'Index') {
         return term === item.index.toString()
     }
     // If an item has the sspecified type, run regex test
-  	if ( !item.hasOwnProperty( type ) ) {
-  		return false
-  	} else {
-  		let regex = new RegExp( term, "gi" )
-    	return regex.test( item[type] )
-  	}
+    if ( !item.hasOwnProperty( type ) ) {
+        return false
+    } else {
+        let regex = new RegExp( term, "gi" )
+        return regex.test( item[type] )
+    }
 }
 
 export default ( item, type, term ) => {
